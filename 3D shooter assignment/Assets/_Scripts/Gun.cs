369d0230a8ca577f06e4d8fb015fd0234a105b16
Kaspa-maker <kaspergutek@gmail.com>
2019-10-03 13:27:20 +0100

    Shooting done

---
+++ 3D shooter assignment/Assets/_Scripts/Gun.cs
@@ -0,0 +1,51 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+    public ParticleSystem MuzzleFlash;
+    public GameObject ImpactEffect;
+    public float force = 30f;
+    public float firerate = 15f;
+
+    private float nextTimeToFire = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire) 
+        {
+            nextTimeToFire = Time.time + 1f / firerate;
+            Shoot();
+        }
+
+
+    }
+
+    void Shoot() 
+    {
+
+        MuzzleFlash.Play();
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null) {
+                hit.rigidbody.AddForce(-hit.normal * force);
+            }
+
+            GameObject impactGO = Instantiate(ImpactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 1f);
+        }
+    }
+}
